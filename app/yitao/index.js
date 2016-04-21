f46de1551fa0f0c95e5d59c256ec8bd21e26b3b6
zsc <2844870322@qq.com>
2016-04-21 19:02:40 +0800

    init

---
+++ app/yitao/index.js
@@ -0,0 +1,4 @@
+angular.module('yitao', [
+  // Declare any module-specific AngularJS dependencies here
+  'common'
+]);
---
+++ app/yitao/scripts/IndexController.js
@@ -0,0 +1,5 @@
+angular
+  .module('yitao')
+  .controller('IndexController', function($scope, supersonic) {
+    // Controller functionality here
+  });
